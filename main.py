ce22413c6f54df3e5f4dbc7496e4eee030360788
Carlos Andres <130174953+CarlosAndresd@users.noreply.github.com>
2023-06-19 18:33:08 +0200

    Added a function to check whether the rectangle can be placed or not

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+
+def enough_space(initial_matrix, ini_row, ini_col, width, height):
+	total_height = np.shape(initial_matrix)[0]
+	total_width = np.shape(initial_matrix)[1]
+
+	if ini_row+height > total_height:
+		return False
+
+	if ini_col+width > total_width:
+		return False
+
+	return True
+
+
 def place_rectangle(initial_matrix, ini_row, ini_col, width, height):
-	new_matrix = np.copy(initial_matrix)
-	new_matrix[ini_row:ini_row+height, ini_col:ini_col+width] = 2*np.ones((height, width))
-	return new_matrix
+
+	if enough_space(initial_matrix, ini_row, ini_col, width, height):
+		new_matrix = np.copy(initial_matrix)
+		new_matrix[ini_row:ini_row+height, ini_col:ini_col+width] = 2*np.ones((height, width))
+		return new_matrix
+	else:
+		print("Not enough space!")
+		return initial_matrix
+
 
 num_rows = 100
 num_cols = 150
