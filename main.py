6f25e72a9d404e38455a270865b2f8e535b3fd73
Carlos Andres <130174953+CarlosAndresd@users.noreply.github.com>
2023-06-22 20:08:11 +0200

    Added the number of rectangles per type and the size of rectangles per type

--- main.py
+++ main.py
@@ -35,26 +35,55 @@ def place_rectangle(initial_matrix, ini_row, ini_col, width, height, coefficient
 	if can_be_placed(initial_matrix, ini_row, ini_col, width, height):
 		new_matrix = np.copy(initial_matrix)
 		new_matrix[ini_row:ini_row+height, ini_col:ini_col+width] = coefficient*np.ones((height, width))
-		return new_matrix
+		return new_matrix, 1
 	else:
-		print("The rectangle cannot be placed")
-		return initial_matrix
+		# print("The rectangle cannot be placed")
+		return initial_matrix, -1
 
 
-num_rows = 100
-num_cols = 150
+num_rows = 200
+num_cols = 300
 
 matrix = np.zeros((num_rows, num_cols))
 
-for _ in range(20):
+rectangle_dimensions_by_type = np.array([[30, 40],
+										[15, 20],
+										[7, 10]])
 
-	rand_row = random.randint(0, num_rows-1)
-	rand_col = random.randint(0, num_cols-1)
 
-	rand_width = random.randint(1, 10)
-	rand_height = random.randint(1, 10)
+number_of_rectangles_by_type = [3, 10, 30]
 
-	matrix = place_rectangle(matrix, rand_row, rand_col, rand_width, rand_height, 1)
+for rectangle_type, number_rectangles in enumerate(number_of_rectangles_by_type):
+
+	print(f"Rectangle type = {rectangle_type}")
+
+	rectangle_width = rectangle_dimensions_by_type[rectangle_type][0]
+	rectangle_height = rectangle_dimensions_by_type[rectangle_type][1]
+
+	for id_rectangle in range(number_rectangles):
+
+		print(f"\tRectangle {id_rectangle} of {number_rectangles}")
+
+		rand_row = random.randint(0, num_rows-1)
+		rand_col = random.randint(0, num_cols-1)
+
+		rectangle_placed = False
+		counter = 0
+
+		while not rectangle_placed:
+
+			counter += 1
+			matrix, status = place_rectangle(matrix, rand_row, rand_col, rectangle_width, rectangle_height, rectangle_type+1)
+			# print(f"Counter = {counter}")
+			# print(f"status = {status}")
+
+			if status == 1:
+				# print("Rectangle placed")
+				rectangle_placed = True
+
+			if counter > 10000:
+				print("Limit reached")
+				rectangle_placed = True
 
 plt.matshow(matrix)
 
