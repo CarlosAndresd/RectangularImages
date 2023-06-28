9176e330ecb02f47673d746ce4fa0064ddecd0c6
Carlos Andres <130174953+CarlosAndresd@users.noreply.github.com>
2023-06-28 22:26:26 +0200

    Added code to read the exif from the photos

--- organise_images.py
+++ organise_images.py
@@ -1,15 +1,32 @@
-from exif import Image
+import os.path, time
+from PIL import Image, ExifTags
 
 folder_path = 'other_images'
-img_filename = 'IMG_9314 Large.jpeg'
+
+# img_filename = '0b0a9861-9f3d-4801-94dd-9c9db7056889.jpg'
+# img_filename = 'IMG_5041.PNG'
+# img_filename = 'IMG_9314 Large.jpeg'
+# img_filename = 'IMG_9314.HEIC'
+# img_filename = 'IMG_1415.HEIC'
+img_filename = 'IMG_1415 Large.jpeg'
+# img_filename = 'IMG_1535.PNG'
+
+
 
 img_path = f'{folder_path}/{img_filename}'
 
-with open(img_path, 'rb') as img_file:
-	img = Image(img_file)
 
-print(img.has_exif)
+print("created: %s" % time.ctime(os.stat(img_path).st_birthtime))
 
-# Original datetime that image was taken (photographed)
-print(f'DateTime (Original): {img.get("datetime_original")}')
+
+img = Image.open(img_path)
+
+image_exif = img._getexif()
+
+if image_exif is not None:
+	exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
+	print(f"Date Time Original = {exif['DateTimeOriginal']}")
+
+else:
+	print('No Exif')
 
