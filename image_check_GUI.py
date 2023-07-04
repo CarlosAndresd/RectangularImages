3ecd78198895fce656f1659cc72af3e7ee69ad5d
Carlos Andres <130174953+CarlosAndresd@users.noreply.github.com>
2023-07-04 09:13:43 +0200

    Added the undo function

--- image_check_GUI.py
+++ image_check_GUI.py
@@ -5,10 +5,11 @@ from os import walk, rename, remove
 import os.path
 import datetime
 
-global image_index, current_image_path, save_images_directory_path, num_images
+global image_index, current_image_path, num_images, movement_records, directory_dictionary
 
 name_source_path = 'images_GUI'
 name_saved_path = 'saved_images'
+name_removed_path = 'removed_images'
 
 
 def create_directory(directory_name, source_path=name_source_path):
@@ -36,10 +37,11 @@ def find_all_files(source_directory_path=name_source_path, copy_file_extensions=
 	return file_names
 
 
-def move_image():
+def move_image(dictionary_key):
 	file_name = os.path.basename(current_image_path)
 	original_file_name, file_extension = os.path.splitext(file_name)
-	new_name = save_images_directory_path + '/' + file_name
+	directory_path = directory_dictionary[dictionary_key]
+	new_name = directory_path + '/' + file_name
 
 	# This is necessary, in case the file already exists
 	copy_counter = 0
@@ -47,13 +49,26 @@ def move_image():
 	while not moved_image:
 		if not os.path.exists(new_name):
 			rename(current_image_path, new_name)
+			movement_records.append([current_image_path, new_name])
+			print(movement_records)
 			moved_image = True
 		else:
 			copy_counter += 1
-			new_name = save_images_directory_path + '/' + original_file_name + '_' + str(copy_counter) + file_extension
+			new_name = directory_path + '/' + original_file_name + '_' + str(copy_counter) + file_extension
 
 
+def undo(event):
+	global image_index, current_image_path
 
+	last_movement = movement_records.pop()
+	old_path = last_movement[1]
+	new_path = last_movement[0]
+	now = datetime.datetime.now()
+	file_log.write('\tUNDO\t\t' + str(now)[:19] + '\t' + old_path + ' -> ' + new_path + '\n')
+	rename(old_path, new_path)
+	image_index -= 1
+	current_image_path = all_images_path[image_index]
+	update_image()
 
 
 def next_image(event):
@@ -62,9 +77,13 @@ def next_image(event):
 	global current_image_path
 
 	now = datetime.datetime.now()
-	file_log.write('\tREMOVED\t\t' + str(now)[:19] + '\t' + current_image_path + '\n')
 
-	remove(current_image_path)
+	if event.keysym == 'Right':
+		file_log.write('\tREMOVED\t\t' + str(now)[:19] + '\t' + current_image_path + '\n')
+		move_image('removed')
+	elif event.keysym == 'Down':
+		file_log.write('\tSAVED\t\t' + str(now)[:19] + '\t' + current_image_path + '\n')
+		move_image('saved')
 
 	image_index += 1
 	if image_index < num_images:
@@ -88,26 +107,6 @@ def no_more_photos():
 	text_input_frame.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
 
-
-def save_image(event):
-
-	global image_index
-	global current_image_path
-
-	now = datetime.datetime.now()
-	file_log.write('\tSAVED\t\t' + str(now)[:19] + '\t' + current_image_path + '\n')
-
-	move_image()
-
-	image_index += 1
-	if image_index < num_images:
-		current_image_path = all_images_path[image_index]
-		update_image()
-		print('save image')
-	else:
-		no_more_photos()
-
-
 def entered_text(event):
 	inp = inputtxt.get(1.0, "end-1c")
 	inp = inp.replace('\n', '')
@@ -120,12 +119,19 @@ def entered_text(event):
 
 
 def start_process():
-	global all_images_path, num_images, image_index, current_image_path
+	global all_images_path, num_images, image_index, current_image_path, movement_records
+	global directory_dictionary
 
 	all_images_path = find_all_files()
 	num_images = len(all_images_path)
 	image_index = 0
 	if len(all_images_path) > 0:
+
+		movement_records = []
+		save_images_directory_path = create_directory(name_saved_path)
+		removed_images_directory_path = create_directory(name_removed_path)
+
+		directory_dictionary = {'saved': save_images_directory_path, 'removed': removed_images_directory_path}
 
 		image_frame.place(relx=0.5, rely=0.4, anchor=tk.CENTER, relwidth=0.7, relheight=0.7)
 		text_input_frame.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
@@ -208,13 +214,14 @@ text_input_frame.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 inputtxt = tk.Text(text_input_frame, height=1, width=20, font=("Helvetica", 40))
 inputtxt.pack()
 inputtxt.bind('<Right>', next_image)
-inputtxt.bind('<Down>', save_image)
+inputtxt.bind('<Left>', undo)
+inputtxt.bind('<Down>', next_image)
 inputtxt.bind('<Return>', entered_text)
 inputtxt.bind("<Shift_L>", window_resize)
 inputtxt.focus()
 
 file_log = open(name_source_path + '/image_changes.txt', 'a')
 
-save_images_directory_path = create_directory(name_saved_path)
+
 
 root.mainloop()
